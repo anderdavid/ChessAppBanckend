70041290129f5ba912dd11391e57633a27d8e816
anderdavid <anderdavid86@gmail.com>
2021-10-19 13:43:59 -0500

    commit inicial

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(8000);
+  console.log('listen by port 8000');
+}
+bootstrap();
