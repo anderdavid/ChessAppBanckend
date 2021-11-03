73a1a59032323fece2c2ef6291cc9010d9fe4d8a
anderdavid <anderdavid86@gmail.com>
2021-11-02 21:00:16 -0500

    Se implementa crud usuarios sin validaciones

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,13 +1,46 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UserService } from './user.service';
+import { User } from './user.entity';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly _userService: UserService) {}
 
+  @Get(':id')
+  async getUser(@Param() id: number) {
+    const users = await this._userService.get(id);
+    return users;
+  }
+
   @Get()
   async getUsers() {
     const users = await this._userService.getAll();
     return users;
+  }
+
+  @Post()
+  async createUser(@Body() user: User) {
+    const createUser = await this._userService.create(user);
+    return createUser;
+  }
+
+  @Patch(':id')
+  async updateUser(@Param() id: number, @Body() user: User) {
+    const updateUser = await this._userService.update(id, user);
+    return updateUser;
+  }
+
+  @Delete()
+  async deleteUser(@Param() id: number) {
+    await this._userService.delete(id);
+    return true;
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotAcceptableException,
+  NotFoundException,
+} from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -10,11 +14,42 @@ export class UserService {
     private readonly _userRepository: UserRepository,
   ) {}
 
+  async get(id: number): Promise<User> {
+    const user = await this._userRepository.findOne(id, {
+      where: { status: 'active' },
+    });
+    if (!user) {
+      throw new NotAcceptableException();
+    }
+    return user;
+  }
+
   async getAll(): Promise<User[]> {
     const users: User[] = await this._userRepository.find({
       where: { status: 'active' },
     });
 
     return users;
+  }
+
+  async create(user: User): Promise<User> {
+    const saveUser: User = await this._userRepository.save(user);
+    return saveUser;
+  }
+
+  async update(id: number, user: User): Promise<any> {
+    const updateUser = await this._userRepository.update(id, user);
+    return updateUser;
+  }
+
+  async delete(id: number): Promise<any> {
+    const userExists = await this._userRepository.findOne(id, {
+      where: { status: 'active' },
+    });
+    if (!userExists) {
+      throw new NotFoundException();
+    }
+    const deleteUser = await this._userRepository.delete(id);
+    return deleteUser;
   }
 }
